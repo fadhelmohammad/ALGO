d41377c60124ee86a3c7dc58891cea36a2ebd06f
MhmmdIchsan <101854401+MhmmdIchsan@users.noreply.github.com>
2022-05-16 20:35:45 +0700

    menambah variabel poin

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
     int pilih,total;
     int poin = 0;
-    int poin1, poin2, poin3, poin4, poin5, poin6, poin7, poin8, poin9, poin10;
+    int poin1, poin2, poin3, poin4, poin5, poin6, poin7, poin8, poin9, poin10, poin11, poin12, poin13, poin14, poin15, poin16, poin17, poin18, poin19, poin20;
     char jawab1, jawab2, jawab3, jawab4, jawab5, jawab6, jawab7, jawab8, jawab9, jawab10;
 
 
