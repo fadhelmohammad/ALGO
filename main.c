3242713e63c3e3da8f89f4143069258586f821a2
rahmi87 <81910399+rahmi87@users.noreply.github.com>
2022-05-16 03:45:28 -0700

    Update main.c

--- main.c
+++ main.c
@@ -110,62 +110,59 @@
     }
       getch();
 
-     printf("\n\nSoal 1.\nRangka tersusun dari?\n");
-     printf("a. Tulang dan daging\nb. Tulang dan otot\nc. Tulang dan kulit\nd. Tulang saja\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab1);
-     if(jawab1 == 'b'){
-       printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
-       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
-       getch();
-
-     printf("\nSoal 2.\nPadi di sawah pak Didi dimakan burung,");
-     printf("lalu burung tersebut\nditangkap untuk menjadi makanan ular. Urutan ini disebut?\n");
-     printf("a. Siklus Kehidupan\nb. Jaring Makanan\nc. Rantai Makanan\nd. Rantai Kehidupan\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab2);
-     if(jawab2 == 'c'){
-       printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
-       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
-       getch();
-
-     printf("\nSoal 3.\nHubungan antara benalu dan pohon mangga membentuk simbiosis?\n");
-     printf("a. Mutualisme\nb. Parasitisme\nc. Komensalisme\nd. Netralisme\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab3);
-     if(jawab3 == 'b'){
-       printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
-       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
-       getch();
-
-     printf("\nSoal 4.\nBenda yang bentuknya berubah-ubah mengikuti wadah,tetapi\nvolumenya tetap disebut?\n");
-     printf("a. Padat\nb. Cair\nc. Gas\nd. Semua benar\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab4);
-     if(jawab4 == 'b'){
-       printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
-       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
-       getch();
-
-    printf("\nSoal 5.\nTelur kupu-kupu menetas menjadi?\n");
-    printf("a. Ulat\nb. Kepompong\nc. Berudu\nd. Pupa\n");
+      printf("\nSoal 11.\nSiapakah yang menjadi khalifah pertama setelah wafatnya Nabi Muhammad SAW?\n");
+    printf("a. Umar bin Khattab\nb. Abu Bakar Ash-Shiddiq\nc. Ali bin Abi Thalib\nd. Ustman bin Affan\n");
     printf("Input jawaban (huruf) : ");
-    scanf(" %c",&jawab5);
-    if(jawab5 == 'a'){
+    scanf(" %c",&jawab11);
+    if(jawab11 == 'b'){
       printf("Hore! Jawaban Anda Benar! >.<\n");
     } else {
       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
     }
-       getch();
 
+    getch();
+     printf("\nSoal 12.\nSiapakah yang menjadi khalifah terakhir dan juga keponakan Nabi Muhammad SAW??\n");
+    printf("a. Ustman bin Affan\nb. Abu Bakar Ash-Shiddiq\nc. Ali bin Abi Thalib\nd. Umar bin Khattab\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab12);
+    if(jawab12 == 'c'){
+      printf("Hore! Jawaban Anda Benar! >.<\n");
+    } else {
+      printf("Ups, Jawaban Anda Kurang Tepat :(\n");
+    }
+
+    getch();
+     printf("\nSoal 13.\nSiapakah khalifah yang paling lama pemerintahannya?\n");
+    printf("a. Ali bin Abi Thalib\nb. Ustman bin Affan\nc. Umar bin Khattab\nd. Abu Bakar Ash-Shiddiq\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab13);
+    if(jawab13 == 'b'){
+      printf("Hore! Jawaban Anda Benar! >.<\n");
+    } else {
+      printf("Ups, Jawaban Anda Kurang Tepat :(\n");
+    }
+
+    getch();
+     printf("\nSoal 14.\nManakah berikut ini yang termasuk ke dalam kelompok hewan ovipar?\n");
+    printf("a. Kucing\nb. Ayam\nc. Beruang\nd. Panda\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab14);
+    if(jawab14 == 'b'){
+      printf("Hore! Jawaban Anda Benar! >.<\n");
+    } else {
+      printf("Ups, Jawaban Anda Kurang Tepat :(\n");
+    }
+
+    getch();
+    printf("\nSoal 15.\nManakah berikut ini yang termasuk ke dalam kelompok hewan karnivora?\n");
+    printf("a. Kucing\nb. Ayam\nc. Beruang\nd. Panda\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab15);
+    if(jawab15 == 'a'){
+      printf("Hore! Jawaban Anda Benar! >.<\n");
+    } else {
+      printf("Ups, Jawaban Anda Kurang Tepat :(\n");
+    }
      printf("\n\nSoal 1.\nRangka tersusun dari?\n");
      printf("a. Tulang dan daging\nb. Tulang dan otot\nc. Tulang dan kulit\nd. Tulang saja\n");
      printf("Input jawaban (huruf) : ");
