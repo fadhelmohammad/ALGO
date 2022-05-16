78d544d4ed5d2bb810b50655d5c4eaadead94c1c
MhmmdIchsan <101854401+MhmmdIchsan@users.noreply.github.com>
2022-05-16 16:18:14 +0700

    mengupdate variabel

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
- 
+ // deklarasi variabel soal
+
+    int pilih,total;
+    int poin = 0;
+    int poin1, poin2, poin3, poin4, poin5, poin6, poin7, poin8, poin9, poin10;
+    char jawab1, jawab2, jawab3, jawab4, jawab5, jawab6, jawab7, jawab8, jawab9, jawab10;
+
+
+  //tampilan awal
+  printf("\n\n================ !QUIZ BERHADIAH! ===============\n");
+  printf("<3 AYO MENJAWAB KUIS---AGAR SEMAKIN PINTAR <3\n\n");
+
+//   welcome();
+    printf("1. Kuis\n");
+    printf("2. Keluar\n");
+
+    printf("Input Pilihan Anda : ");
+    scanf("%d", &pilih);
+    getchar();
