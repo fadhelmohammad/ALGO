a7cd9ed3e7fdb678916aed10fb2f177267c7aabc
rahmi87 <81910399+rahmi87@users.noreply.github.com>
2022-05-16 04:12:17 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,46 +1,46 @@
-     printf("\n\nSoal 1.\nRangka tersusun dari?\n");
-     printf("a. Tulang dan daging\nb. Tulang dan otot\nc. Tulang dan kulit\nd. Tulang saja\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab1);
-     if(jawab1 == 'b'){
-       printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
+    printf("\n\nSoal 1.\nRangka tersusun dari?\n");
+    printf("a. Tulang dan daging\nb. Tulang dan otot\nc. Tulang dan kulit\nd. Tulang saja\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab1);
+    if(jawab1 == 'b'){
+        printf("Hore! Jawaban Anda Benar! >.<\n");
+    } else {
        printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
-       getch();
+    }
+        getch();
 
-     printf("\nSoal 2.\nPadi di sawah pak Didi dimakan burung,");
-     printf("lalu burung tersebut\nditangkap untuk menjadi makanan ular. Urutan ini disebut?\n");
-     printf("a. Siklus Kehidupan\nb. Jaring Makanan\nc. Rantai Makanan\nd. Rantai Kehidupan\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab2);
-     if(jawab2 == 'c'){
+    printf("\nSoal 2.\nPadi di sawah pak Didi dimakan burung,");
+    printf("lalu burung tersebut\nditangkap untuk menjadi makanan ular. Urutan ini disebut?\n");
+    printf("a. Siklus Kehidupan\nb. Jaring Makanan\nc. Rantai Makanan\nd. Rantai Kehidupan\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab2);
+    if(jawab2 == 'c'){
        printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
+    } else {
        printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
-       getch();
+    }
+        getch();
 
-     printf("\nSoal 3.\nHubungan antara benalu dan pohon mangga membentuk simbiosis?\n");
-     printf("a. Mutualisme\nb. Parasitisme\nc. Komensalisme\nd. Netralisme\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab3);
-     if(jawab3 == 'b'){
+    printf("\nSoal 3.\nHubungan antara benalu dan pohon mangga membentuk simbiosis?\n");
+    printf("a. Mutualisme\nb. Parasitisme\nc. Komensalisme\nd. Netralisme\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab3);
+    if(jawab3 == 'b'){
        printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
+    } else {
        printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
-       getch();
+    }
+        getch();
 
-     printf("\nSoal 4.\nBenda yang bentuknya berubah-ubah mengikuti wadah,tetapi\nvolumenya tetap disebut?\n");
-     printf("a. Padat\nb. Cair\nc. Gas\nd. Semua benar\n");
-     printf("Input jawaban (huruf) : ");
-     scanf(" %c",&jawab4);
-     if(jawab4 == 'b'){
+    printf("\nSoal 4.\nBenda yang bentuknya berubah-ubah mengikuti wadah,tetapi\nvolumenya tetap disebut?\n");
+    printf("a. Padat\nb. Cair\nc. Gas\nd. Semua benar\n");
+    printf("Input jawaban (huruf) : ");
+    scanf(" %c",&jawab4);
+    if(jawab4 == 'b'){
        printf("Hore! Jawaban Anda Benar! >.<\n");
-     } else {
+    } else {
        printf("Ups, Jawaban Anda Kurang Tepat :(\n");
-     }
+    }
        getch();
 
     printf("\nSoal 5.\nTelur kupu-kupu menetas menjadi?\n");
@@ -52,7 +52,7 @@
     } else {
       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
     }
-       getch();
+        getch();
 
     printf("\nSoal 6.\nHewan yang memiliki metamorfosis sempurna adalah?\n");
     printf("a. Ular\nb. Kuda\nc. Kupu-kupu\nd. Kucing\n");
@@ -63,7 +63,7 @@
     } else {
       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
     }
-       getch();
+        getch();
 
     printf("\nSoal 7.\nYang termasuk hewan omnivora adalah?\n");
     printf("a. Beruang\nb. Kupu-kupu\nc. Domba\nd. Sapi\n");
@@ -74,7 +74,7 @@
     } else {
       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
     }
-       getch();
+        getch();
 
     printf("\nSoal 8.\n2 + 2 x 0?\n");
     printf("a. 0\nb. 4\nc. a dan d benar\nd. 2\n");
@@ -85,7 +85,7 @@
     } else {
       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
     }
-      getch();
+        getch();
 
     printf("\nSoal 9.\nIbu Kota Sulawesi Tenggara?\n");
     printf("a. Kendari\nb. Banda Aceh\nc. Kupang\nd. Ambon\n");
@@ -96,7 +96,7 @@
     } else {
       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
     }
-      getch();
+        getch();
 
     printf("\nSoal 10.\nTokoh di Attack on Titan?\n");
     printf("a. Upin\nb. Levi\nc. Nobita\nd. Saitama\n");
@@ -107,7 +107,7 @@
     } else {
       printf("Ups, Jawaban Anda Kurang Tepat :(\n");
     }
-      getch();
+        getch();
 
     printf("\nSoal 11.\nSiapakah yang menjadi khalifah pertama setelah wafatnya Nabi Muhammad SAW?\n");
     printf("a. Umar bin Khattab\nb. Abu Bakar Ash-Shiddiq\nc. Ali bin Abi Thalib\nd. Ustman bin Affan\n");
